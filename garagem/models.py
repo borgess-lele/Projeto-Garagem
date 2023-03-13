4afee13a32c16f77813907d643b7baf8c66186fb
Leticia <leticiaborgess.oficial@gmail.com>
2023-03-13 16:42:21 -0300

    Criação do Modelo Marca

--- garagem/models.py
+++ garagem/models.py
@@ -5,3 +5,12 @@ class Categoria(models.Model):
 
     def __str__(self):
         return self.descricao
+    
+    ...
+
+class Marca(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
