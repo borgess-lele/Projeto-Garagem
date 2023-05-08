d3b01fc2aab133c31c8aee9e96bff27211640d37
leticia <leticiaborgess.oficial@gmail.com>
2023-05-08 16:44:39 -0300

    Acessorio

--- garagem/views.py
+++ garagem/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from garagem.models import Categoria, Cor, Acessorio
+from garagem.serializers import CategoriaSerializer, CorSerializer, AcessorioSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class CorViewSet(ModelViewSet):
+    queryset = Cor.objects.all()
+    serializer_class = CorSerializer
+
+class AcessorioViewSet(ModelViewSet):
+    queryset = Acessorio.objects.all()
+    serializer_class = AcessorioSerializer
